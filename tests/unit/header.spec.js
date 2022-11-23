f78a0d025a68e6708667082c6386257f55e7dd51
mahmoud essam <esame4166@gmail.com>
2022-11-23 02:55:55 +0200

    added the first test unit

---
+++ tests/unit/header.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from "@vue/test-utils";
+import TheHeader from "@/components/TheHeader.vue";
+
+describe("TheHeader.vue", () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(TheHeader, {
+    });
+  });
+
+  it("renders header component", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+});
